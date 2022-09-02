003bc9ca75213525fc69ae4a4131c8a2f76b6446
Raphael Anizio da Silva <96752946+raphaelaniziodasilva@users.noreply.github.com>
2022-09-02 17:11:33 -0300

    Comentarios

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -15,7 +15,10 @@ export class RoomController {
 		// não se esqueça de criar na pasta do repositories o arquivo romRepository
 
 		try { // vamos criar um repositorio no banco de dados, criando uma nova aula
-			const newRoom = roomRepository.create({ name, description })
+			const newRoom = roomRepository.create(
+			{ 	name, 
+				description 
+			})
 
 			// salvando a nova aula no banco de dados
 			await roomRepository.save(newRoom)
@@ -28,13 +31,13 @@ export class RoomController {
 		}
 	}
 
-	// criando o video para a aula e adicionando na aula PARAMOS EM 1:38:00
+	// criando o video para a aula e adicionando na aula
 	async createVideo(req: Request, res: Response) {
 
 		// pegando do body 
 		const { title, url } = req.body
 
-		// pegando do parametro de rota
+		// pegando do parametro de rota aonde vamos ter que passar o id da aula para poder adicionar o video a ela
 		const { idRoom } = req.params
 
 		try { // verficando se a aula ja existe
@@ -43,7 +46,7 @@ export class RoomController {
 				return res.status(404).json({ message: 'Aula não existe' })
 			}
 
-			// adicionando um video para a aula
+			// criando o video e adicionando o video para a aula
 			const newVideo = videoRepository.create({
 				title,
 				url,
@@ -52,8 +55,10 @@ export class RoomController {
 				room, 
 			})
 
+			// salvando o video no banco de dados
 			await videoRepository.save(newVideo)
 
+			// retornando os dados do video criado
 			return res.status(201).json(newVideo)
 		} catch (error) {
 			console.log(error)
@@ -61,50 +66,67 @@ export class RoomController {
 		}
 	}
 
-	// criando disciplina e adicionando na aula
+	// adicionando uma disciplina na aula
 	async roomSubject(req: Request, res: Response) {
+
+		// subject_id vai ser o id da disciplina que vai ser adicionado
 		const { subject_id } = req.body
+
+		// pegando do parametro de rota aonde vamos ter que passar o id da aula para poder adicionar a disciplina a ela
 		const { idRoom } = req.params
 
 		try {
+			// verificando se o romm: aula existe
 			const room = await roomRepository.findOneBy({ id: Number(idRoom) })
-
 			if (!room) {
 				return res.status(404).json({ message: 'Aula não existe' })
 			}
 
+			// pegando a disciplina do repository: banco de dados
 			const subject = await subjectRepository.findOneBy({
 				id: Number(subject_id),
 			})
 
+			// se a discipina não existir não consigo adicionar nada 
 			if (!subject) {
 				return res.status(404).json({ message: 'Disciplina não existe' })
 			}
 
+			// adicinando a disciplina na aula
 			const roomUpdate = {
+				// pegando todo o objeto que tem dentro de  romm
 				...room,
-				subjects: [subject],
+
+				// aperte ctrl + espaço para trazer a lista do que quer adicionar
+
+				subjects: [subject], // array para poder adicionar varias disciplinas para a aula
 			}
 
+			// adicionando a disciplina no banco de dados
 			await roomRepository.save(roomUpdate)
 
-			return res.status(204).send()
+			return res.status(200).json(room)
 		} catch (error) {
 			console.log(error)
 			return res.status(500).json({ message: 'Internal Sever Error' })
 		}
 	}
 
+	// listando todas as aulas e videos
 	async list(req: Request, res: Response) {
 		try {
+			// pegando todas as aulas
 			const rooms = await roomRepository.find({
-				relations: {
+
+				// vou dizer quais relacionamentos que eu quero trazer dessa aula 	
+				relations: { 						
 					subjects: true,
 					videos: true,
+				
 				},
 			})
 
-			return res.json(rooms)
+			return res.status(200).json(rooms)
 		} catch (error) {
 			console.log(error)
 			return res.status(500).json({ message: 'Internal Sever Error' })
--- src/routes.ts
+++ src/routes.ts
@@ -12,13 +12,16 @@ routes.post('/subject', new SubjectController().create)
 // criando sala, instanciando a classe RoomController e dentro dela pegando o metodo create para passar como controlador desse EndPoint
 routes.post('/room', new RoomController().create)
 
-routes.get('/room', new RoomController().list)
-
 // criando video e adicionando na aula, instanciando a classe RoomController e dentro dela pegando o metodo createVideo para passar como controlador desse EndPoint
+// No postaman vamos ter dizer qual e o id da aula que vai receber o video
 routes.post('/room/:idRoom/create', new RoomController().createVideo)
 
-// criando uma disciplina e adicionando na aula
+// criando uma disciplina e adicionando na aula, instanciando a classe RoomController e dentro dela pegando o metodo roomSubject para passar como controlador desse EndPoint
+// No postaman vamos ter dizer qual e o id da aula que vai receber a disciplina
 routes.post('/room/:idRoom/subject', new RoomController().roomSubject)
+
+// listando todas as aulas, instanciando a classe RoomController e dentro dela pegando o metodo list para passar como controlador desse EndPoint
+routes.get('/room', new RoomController().list)
 
 // exportando o routes para poder chamar elas la no index.ts    
 export default routes
